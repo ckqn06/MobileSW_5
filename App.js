60ebb443b564073da702a7691ffd2aa690cd81f1
ckqn06 <ckqn06@naver.com>
2022-11-03 15:47:59 +0900

    테스트

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native'
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Mobile SW~</Text>
+      <Text>안녕하세요</Text>
     </View>
   )
 }
