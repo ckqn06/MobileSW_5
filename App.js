7bc7952708f9a99ad53ff4249b369344521e0df9
강태영 <kty1467@naver.com>
2022-11-03 14:42:09 +0900

    test commit

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from 'react-native'
 const App = () => {
   return (
     <View style={styles.container}>
-      <Text>Starting App</Text>
+      <Text>test</Text>
     </View>
   )
 }
