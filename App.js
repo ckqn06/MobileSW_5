de492acd366b7728916b2da7d8943a403aab226b
ckqn06 <ckqn06@naver.com>
2022-11-15 21:23:19 +0900

    임시 커밋

--- App.js
+++ App.js
@@ -1,7 +1,49 @@
 import { View, Text, StyleSheet } from 'react-native'
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
-import Start from './Screen/Start';
+
+import Start from './Screen/Start'
+import Register from './Screen/Register'
+import QuizList from './Screen/QuizList'
+import Quiz1 from './Screen/Quiz1'
+import Quiz2 from './Screen/Quiz2'
+import Quiz3 from './Screen/Quiz3'
+import Quiz4 from './Screen/Quiz4'
+import Quiz5 from './Screen/Quiz5'
+import Quiz6 from './Screen/Quiz6'
+import Quiz7 from './Screen/Quiz7'
+import Quiz8 from './Screen/Quiz8'
+import Strate1_1 from './Screen/Strate1_1'
+import Strate1_2 from './Screen/Strate1_2'
+import Strate1_3 from './Screen/Strate1_3'
+
+import Strate2_1 from './Screen/Strate2_1'
+import Strate2_2 from './Screen/Strate2_2'
+import Strate2_3 from './Screen/Strate2_3'
+
+import Strate3_1 from './Screen/Strate3_1'
+import Strate3_2 from './Screen/Strate3_2'
+import Strate3_3 from './Screen/Strate3_3'
+
+import Strate4_1 from './Screen/Strate4_1'
+import Strate4_2 from './Screen/Strate4_2'
+import Strate4_3 from './Screen/Strate4_3'
+
+import Strate5_1 from './Screen/Strate5_1'
+import Strate5_2 from './Screen/Strate5_2'
+import Strate5_3 from './Screen/Strate5_3'
+
+import Strate6_1 from './Screen/Strate6_1'
+import Strate6_2 from './Screen/Strate6_2'
+import Strate6_3 from './Screen/Strate6_3'
+
+import Strate7_1 from './Screen/Strate7_1'
+import Strate7_2 from './Screen/Strate7_2'
+import Strate7_3 from './Screen/Strate7_3'
+
+import Strate8_1 from './Screen/Strate8_1'
+import Strate8_2 from './Screen/Strate8_2'
+import Strate8_3 from './Screen/Strate8_3'
 
 const Stack = createStackNavigator();
 export default function App() {
@@ -9,6 +51,47 @@ export default function App() {
     <NavigationContainer>
       <Stack.Navigator>
         <Stack.Screen name = "Start" component={Start}/>
+        <Stack.Screen name = "Register" component={Register}/>
+        <Stack.Screen name = "QuizList" component={QuizList}/>
+        <Stack.Screen name = "Quiz1" component={Quiz1}/>
+        <Stack.Screen name = "Quiz2" component={Quiz2}/>
+        <Stack.Screen name = "Quiz3" component={Quiz3}/>
+        <Stack.Screen name = "Quiz4" component={Quiz4}/>
+        <Stack.Screen name = "Quiz5" component={Quiz5}/>
+        <Stack.Screen name = "Quiz6" component={Quiz6}/>
+        <Stack.Screen name = "Quiz7" component={Quiz7}/>
+        <Stack.Screen name = "Quiz8" component={Quiz8}/>
+        <Stack.Screen name = "Strate1_1" component={Strate1_1}/>
+        <Stack.Screen name = "Strate1_2" component={Strate1_2}/>
+        <Stack.Screen name = "Strate1_3" component={Strate1_3}/>
+        
+        <Stack.Screen name = "Strate2_1" component={Strate2_1}/>
+        <Stack.Screen name = "Strate2_2" component={Strate2_2}/>
+        <Stack.Screen name = "Strate2_3" component={Strate2_3}/>
+
+        <Stack.Screen name = "Strate3_1" component={Strate3_1}/>
+        <Stack.Screen name = "Strate3_2" component={Strate3_2}/>
+        <Stack.Screen name = "Strate3_3" component={Strate3_3}/>
+
+        <Stack.Screen name = "Strate4_1" component={Strate4_1}/> 
+        <Stack.Screen name = "Strate4_2" component={Strate4_2}/>
+        <Stack.Screen name = "Strate4_3" component={Strate4_3}/> 
+
+        <Stack.Screen name = "Strate5_1" component={Strate5_1}/>
+        <Stack.Screen name = "Strate5_2" component={Strate5_2}/>
+        <Stack.Screen name = "Strate5_3" component={Strate5_3}/>
+
+        <Stack.Screen name = "Strate6_1" component={Strate6_1}/>
+        <Stack.Screen name = "Strate6_2" component={Strate6_2}/>
+        <Stack.Screen name = "Strate6_3" component={Strate6_3}/>
+
+        <Stack.Screen name = "Strate7_1" component={Strate7_1}/>
+        <Stack.Screen name = "Strate7_2" component={Strate7_2}/>
+        <Stack.Screen name = "Strate7_3" component={Strate7_3}/>
+
+        <Stack.Screen name = "Strate8_1" component={Strate8_1}/>
+        <Stack.Screen name = "Strate8_2" component={Strate8_2}/>
+        <Stack.Screen name = "Strate8_3" component={Strate8_3}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ screen/Strate6_2.js
@@ -0,0 +1,223 @@
+import { View, Text, Button, StyleSheet, TextInput,
+    KeyboardAvoidingView, ScrollView,
+    Platform, Keyboard, TouchableWithoutFeedback} from "react-native";
+import {useState} from 'react'
+
+
+const Strate6_2 = (props) => {
+    const [show1, setShow1] = useState(false);              //2번째 화면 상태 값 default는 false로 동작
+    const [show2, setShow2] = useState(false);              //3번째 화면 상태 값 default는 false로 동작
+    const [show3, setShow3] = useState(false);              //4번째 화면 상태 값 default는 false로 동작
+    const [myTextInput1, setMyTextInput1] = useState("")    //1번 답 저장 하는 공간
+    const [myTextInput2, setMyTextInput2] = useState("")    //2번 답 저장 하는 공간
+    const [myTextInput3, setMyTextInput3] = useState("")    //3번 답 저장 하는 공간
+    const [myTextInput4, setMyTextInput4] = useState("")    //4번 답 저장 하는 공간
+    const onChangeInput1 = (event) => {
+        setMyTextInput1(event)
+    }
+    const onChangeInput2 = (event) => {
+        setMyTextInput2(event)
+    }
+    const onChangeInput3 = (event) => {
+        setMyTextInput3(event)
+    }
+    const onChangeInput4 = (event) => {
+        setMyTextInput4(event)
+    }
+    const correct1 = () => {
+        if (myTextInput1 == 11) {
+            alert("next");
+            setShow1(true)
+        } else {
+            alert("miss");
+        }
+    }
+    const correct2 = () => {
+        if (myTextInput2 == 11) {
+            alert("next");
+            setShow2(true)
+        } else {
+            alert("miss");
+        }
+    }
+    const correct3 = () => {
+        if (myTextInput3 == 11) {
+            alert("next");
+            setShow3(true)
+        } else {
+            alert("miss");
+        }
+    }
+    const correct4 = () => {
+        if (myTextInput4 == 11) {
+            alert("Nice! The width of the rectangle 20. Let’s try a different method!");
+            props.navigation.navigate("Quiz6")
+        } else {
+            alert("miss");
+        }
+    }
+    return (
+        <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+        > 
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+        <ScrollView style ={{width:"100%"}}>    
+            <View style = {styles.container}>
+                <View style = {styles.textcon}>
+                <Text style = {styles.text}>Ok, you want to guess and check. 
+                How long do you think the width is?
+                </Text>
+                <TextInput
+                style = {styles.input}
+                placeholder="answer"
+                value = {myTextInput1}
+                onChangeText = {onChangeInput1}
+                />
+                </View >
+                <View style = {styles.button}>
+                    <Button
+                        title = "check"
+                        onPress = {correct1}
+                    />
+                </View>
+                {/* 조건 연산자 사용 Ex. {조건 ? view : null} 초기 값이 false이기 때문에 null값이 선택되었다가 true로 바뀌면 view출력 */}
+                {show1 ? (
+                <View>
+                    <View style = {styles.textcon2}>
+                    <Text style = {styles.text}>
+                    Cool, now let’s also guess the length of this rectangle.
+                    </Text>
+                    <TextInput
+                    style = {styles.input}
+                    placeholder="answer."
+                    value = {myTextInput2}
+                    onChangeText = {onChangeInput2}
+                    />
+                    </View>
+                    <View style = {styles.button}>
+                        <Button
+                            title = "check"
+                            onPress = {correct2}
+                        />
+                    </View>
+                </View>
+                ): null}
+                {show2 ? (
+                    <View>
+                    <View style = {styles.textcon3}>
+                    <Text style = {styles.text}>
+                    Ok, so you guessed that the width is [w] and the length is [w+12]. 
+                    Now let’s find the perimeter of the rectangle with these dimensions.
+                    </Text>
+                    <TextInput
+                    style = {styles.input}
+                    placeholder="answer."
+                    value = {myTextInput3}
+                    onChangeText = {onChangeInput3}
+                    />
+                    </View>
+                    <View style = {styles.button}>
+                        <Button
+                            title = "check"
+                            onPress = {correct3}
+                        />
+                    </View>
+                </View>
+                ):null}
+                {show3 ? (
+                    <View>
+                    <View style = {styles.textcon4}>
+                    <Text style = {styles.text}>
+                    Great! You got a perimeter of 104 inches. Now remember what you guessed. 
+                    What is the width of the rectangle?
+                    </Text>
+                    <TextInput
+                    style = {styles.input}
+                    placeholder="answer."
+                    value = {myTextInput4}
+                    onChangeText = {onChangeInput4}
+                    />
+                    </View>
+                    <View style = {styles.button}>
+                        <Button
+                            title = "check"
+                            onPress = {correct4}
+                        />
+                    </View>
+                </View>
+                ):null}
+            </View>
+            </ScrollView>
+        </TouchableWithoutFeedback> 
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#DDA0DD',
+        paddingTop: 30,
+        paddingBottom: 30
+    },
+    button: {
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 10,
+        marginTop: 20
+    },
+    textcon: {
+        backgroundColor: 'white',
+        height: 80,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 30,
+        marginTop: 10,
+        borderRadius: 3,
+    },
+    textcon2: {
+        backgroundColor: 'white',
+        height: 60,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 30,
+        marginTop: 10,
+        borderRadius: 3,
+    },
+    textcon3: {
+        backgroundColor: 'white',
+        height: 100,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 30,
+        marginTop: 10,
+        borderRadius: 3,
+    },
+    textcon4: {
+        backgroundColor: 'white',
+        height: 100,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 30,
+        marginTop: 10,
+        borderRadius: 3,
+    },
+    text: {
+        fontSize: 20,
+        color: 'black',
+    },
+    tt: {
+        backgroundColor: '#DDA0DD',
+        color: 'black',
+        marginBottom: 5,
+    },
+    input: {
+        marginTop: 15,
+        marginBottom: 15,
+        backgroundColor: 'white',
+
+    },
+}); 
+
+export default Strate6_2
