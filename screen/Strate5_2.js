6f8016d030eb925274d80c34cd56fb894300355f
aqwsde1 <75656976+aqwsde1@users.noreply.github.com>
2022-11-16 22:36:03 +0900

    Add files via upload

---
+++ screen/Strate5_2.js
@@ -0,0 +1,122 @@
+import { View, Text, Button, StyleSheet, TextInput,
+    KeyboardAvoidingView,
+    Platform, Keyboard, TouchableWithoutFeedback} from "react-native";
+import {useState} from 'react'
+
+const Strate5_2 = (props) => {
+    const [show, setShow] = useState(false);                //2번째 화면 상태 값 default는 false로 동작
+    const [myTextInput1, setMyTextInput1] = useState("")    //1번 답 저장 하는 공간
+    const [myTextInput2, setMyTextInput2] = useState("")    //2번 답 저장 하는 공간
+    const onChangeInput1 = (event) => {
+        setMyTextInput1(event)
+    }
+    const onChangeInput2 = (event) => {
+        setMyTextInput2(event)
+    }
+    const correct1 = () => {
+        if (myTextInput1 == 11) {
+            alert("next");
+            setShow(true)
+        } else {
+            alert("miss");
+        }
+    }
+    const correct2 = () => {
+        if (myTextInput2 == 11) {
+            alert("Nice! Mario can cut 6 sections of rope. Let’s try a different method!");
+            props.navigation.navigate("Quiz5")
+        } else {
+            alert("miss");
+        }
+    }
+    return (
+        <KeyboardAvoidingView
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+        style={styles.container}
+        > 
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>    
+            <View style = {styles.container}>
+                <View style = {styles.textcon}>
+                <Text style = {styles.text}>Ok, you want to add on sections to 34.5 inches until you use up all the rope. 
+                First, how many inches of rope does Mario have?
+                </Text>
+                <TextInput
+                style = {styles.input}
+                placeholder="answer"
+                value = {myTextInput1}
+                onChangeText = {onChangeInput1}
+                />
+                </View >
+                <View style = {styles.button}>
+                    <Button
+                        title = "check"
+                        onPress = {correct1}
+                    />
+                </View>
+                {/* 조건 연산자 사용 Ex. {조건 ? view : null} 초기 값이 false이기 때문에 null값이 선택되었다가 true로 바뀌면 view출력 */}
+                {show ? (
+                <View>
+                    <View style = {styles.textcon}>
+                    <Text style = {styles.text}>How many times can you add 8¼ inches to 34.5 inches to get 84 inches?
+                    </Text>
+                    <TextInput
+                    style = {styles.input}
+                    placeholder="answer."
+                    value = {myTextInput2}
+                    onChangeText = {onChangeInput2}
+                    />
+                    </View>
+                    <View style = {styles.button}>
+                        <Button
+                            title = "check"
+                            onPress = {correct2}
+                        />
+                    </View>
+                </View>
+                ): null}
+            </View>
+        </TouchableWithoutFeedback> 
+        </KeyboardAvoidingView>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor: '#DDA0DD',
+        paddingTop: 30,
+        paddingBottom: 30
+    },
+    button: {
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 10,
+        marginTop: 20
+    },
+    textcon: {
+        backgroundColor: 'white',
+        height: 150,
+        marginLeft: 30,
+        marginRight: 30,
+        marginBottom: 30,
+        marginTop: 10,
+        borderRadius: 3,
+    },
+    text: {
+        fontSize: 20,
+        color: 'black',
+    },
+    tt: {
+        backgroundColor: '#DDA0DD',
+        color: 'black',
+        marginBottom: 5,
+    },
+    input: {
+        marginTop: 15,
+        marginBottom: 15,
+        backgroundColor: 'white',
+
+    },
+}); 
+
+export default Strate5_2
